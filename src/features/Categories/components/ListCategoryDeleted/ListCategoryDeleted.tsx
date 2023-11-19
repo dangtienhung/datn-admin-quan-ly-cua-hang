fac135576ae766f4e6d68aee8dfa2aa6dc0e8f8e
cô giáo hưng <hungdang02042003@gmail.com>
2023-11-19 21:28:55 +0700

    feat: get all categories role staff

--- src/features/Categories/components/ListCategoryDeleted/ListCategoryDeleted.tsx
+++ src/features/Categories/components/ListCategoryDeleted/ListCategoryDeleted.tsx
@@ -1,38 +1,23 @@
-import Loading from '~/components/Loading/Loading'
-import { BsFillTrashFill } from 'react-icons/bs'
-import { RedoOutlined } from '@ant-design/icons'
-import { Popconfirm, Space, Table, Button as ButtonAnt, Tooltip } from 'antd'
-import { ColumnsType } from 'antd/es/table'
-import { ICategory } from '~/types'
-import { NotFound } from '~/pages'
+import { Button as ButtonAnt, Popconfirm, Space, Table } from 'antd'
+import { ICategory, IRoleUser } from '~/types'
 import { useDeleteRealMutation, useGetAllCategoryDeletedQuery, useRestoreCategoryMutation } from '~/store/services'
-import { useState } from 'react'
-import { pause } from '~/utils/pause'
-import { messageAlert } from '~/utils/messageAlert'
+
+import { RootState } from '~/store/store'
 import { cancelDelete } from '../..'
+import { messageAlert } from '~/utils/messageAlert'
+import { useAppSelector } from '~/store/hooks'
+import { useRenderCategory } from '../../hooks'
+import { useState } from 'react'
 
 const ListCategoryDeleted = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([])
-  const { data: categories, isError, isLoading } = useGetAllCategoryDeletedQuery(currentPage)
+  const { data: categories } = useGetAllCategoryDeletedQuery(currentPage)
   const [deleteRealCategory] = useDeleteRealMutation()
   const [restoreCategory] = useRestoreCategoryMutation()
 
-  const handleRestore = async (id: string) => {
-    await pause(2000)
-    restoreCategory(id)
-      .unwrap()
-      .then(() => messageAlert('Khôi phục thành công', 'success'))
-      .catch(() => messageAlert('Khôi phục thất bại', 'error'))
-  }
+  const { user } = useAppSelector((state: RootState) => state.persistedReducer.auth)
 
-  const handleDeleteReal = async (id: string) => {
-    await pause(2000)
-    deleteRealCategory(id)
-      .unwrap()
-      .then(() => messageAlert('Xóa thành công', 'success'))
-      .catch(() => messageAlert('Xóa thất bại', 'error'))
-  }
   const handleDeleteMany = () => {
     selectedRowKeys.forEach((selectedItem) => {
       deleteRealCategory(selectedItem as string)
@@ -64,104 +49,54 @@ const ListCategoryDeleted = () => {
     onChange: onSelectChange
   }
   const hasSelected = selectedRowKeys.length > 1
-  const columns: ColumnsType<ICategory> = [
-    {
-      title: '#',
-      dataIndex: 'index',
-      width: 50
-    },
-    {
-      title: 'Tên danh mục',
-      dataIndex: 'name',
-      key: 'name',
-      render: (name: string) => <span className='capitalize'>{name}</span>
-    },
 
-    {
-      // title: <span className='block text-center'>Action</span>,
-      key: 'action',
-      // fixed: 'right',
-      width: 200,
-      render: (_, category) => (
-        <div className='flex items-center justify-center'>
-          <Space size='middle'>
-            <Tooltip title='Khôi phục danh mục này'>
-              <Popconfirm
-                title='Bạn muốn khôi phục lại danh mục này?'
-                description='Bạn thực sự muốn khôi phục lại danh mục này?'
-                onConfirm={() => handleRestore(category._id)}
-              >
-                <ButtonAnt
-                  size='large'
-                  className='bg-primary hover:!text-white flex items-center justify-center text-white'
-                  icon={<RedoOutlined className='text-lg' />}
-                />
-              </Popconfirm>
-            </Tooltip>
-            <Tooltip title='Xóa vĩnh viễn danh mục này'>
-              <Popconfirm
-                title='Bạn có muốn xóa VĨNH VIỄN danh mục này?'
-                description='Hành động này sẽ không thể khôi phục lại!'
-                okButtonProps={{ style: { backgroundColor: '#3C50E0', color: '#fff' } }}
-                // onCancel={cancelDelete}
-                onConfirm={() => handleDeleteReal(category._id)}
-              >
-                <ButtonAnt
-                  size='large'
-                  className='bg-meta-1 hover:!text-white flex items-center justify-center text-white'
-                  icon={<BsFillTrashFill />}
-                />
-              </Popconfirm>
-            </Tooltip>
-          </Space>
-        </div>
-      )
-    }
-  ]
   const categorriesData = categories?.docs.map((item: ICategory, index) => ({
     ...item,
     key: item._id,
     index: index + 1
   }))
 
-  if (isLoading) return <Loading />
-  if (isError) return <NotFound />
+  const columnsData = useRenderCategory(categories?.docs || [], true)
+
   return (
     <>
-      <Space className='mb-4'>
-        <Popconfirm
-          title='Bạn thực sự muốn khôi phục những danh mục này?'
-          description='Hành động này sẽ khôi phục những danh mục đang được chọn!'
-          onConfirm={handleRestoreMany}
-          onCancel={() => setSelectedRowKeys([])}
-        >
-          <ButtonAnt
-            disabled={!hasSelected}
-            size='large'
-            className='bg-primary hover:!text-white flex items-center justify-center text-white'
+      {user && user.role === IRoleUser.ADMIN && (
+        <Space className='mb-4'>
+          <Popconfirm
+            title='Bạn thực sự muốn khôi phục những danh mục này?'
+            description='Hành động này sẽ khôi phục những danh mục đang được chọn!'
+            onConfirm={handleRestoreMany}
+            onCancel={() => setSelectedRowKeys([])}
           >
-            Khôi phục tất cả
-          </ButtonAnt>
-        </Popconfirm>
-        <Popconfirm
-          title='Bạn thực sự muốn xóa VĨNH VIỄN những danh mục này?'
-          description='Hành động này sẽ xóa những danh mục đang được chọn!'
-          onConfirm={handleDeleteMany}
-          onCancel={() => setSelectedRowKeys([])}
-        >
-          <ButtonAnt
-            disabled={!hasSelected}
-            size='large'
-            className='bg-meta-1 hover:!text-white flex items-center justify-center text-white'
+            <ButtonAnt
+              disabled={!hasSelected}
+              size='large'
+              className='bg-primary hover:!text-white flex items-center justify-center text-white'
+            >
+              Khôi phục tất cả
+            </ButtonAnt>
+          </Popconfirm>
+          <Popconfirm
+            title='Bạn thực sự muốn xóa VĨNH VIỄN những danh mục này?'
+            description='Hành động này sẽ xóa những danh mục đang được chọn!'
+            onConfirm={handleDeleteMany}
+            onCancel={() => setSelectedRowKeys([])}
           >
-            Xóa tất cả
-          </ButtonAnt>
-        </Popconfirm>
-      </Space>
+            <ButtonAnt
+              disabled={!hasSelected}
+              size='large'
+              className='bg-meta-1 hover:!text-white flex items-center justify-center text-white'
+            >
+              Xóa tất cả
+            </ButtonAnt>
+          </Popconfirm>
+        </Space>
+      )}
 
       <div className='dark:bg-graydark w-full overflow-x-auto'>
         <Table
-          columns={columns}
+          // columns={columns}
+          columns={columnsData}
           dataSource={categorriesData}
           pagination={{
             pageSize: categories && categories.limit,
@@ -174,7 +109,7 @@ const ListCategoryDeleted = () => {
           }}
           scroll={{ y: '50vh', x: 650 }}
           bordered
-          rowSelection={rowSelection}
+          rowSelection={user.role === IRoleUser.ADMIN ? rowSelection : undefined}
         />
       </div>
     </>
