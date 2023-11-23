e49f701088066e0dedbea6bb1b96684e5245a0ea
Quanghwi <huydqph24532@fpt.edu.vn>
2023-11-23 11:53:18 -0800

    Search blog categories

--- src/features/CategoriesBlog/hooks/useRenderCategoryBlog.tsx
+++ src/features/CategoriesBlog/hooks/useRenderCategoryBlog.tsx
@@ -1,5 +1,5 @@
 import { BsFillPencilFill, BsFillTrashFill } from 'react-icons/bs'
-import { Button as ButtonAntd, Popconfirm, Space, Tooltip } from 'antd'
+import { Button as ButtonAntd, Input, InputRef, Popconfirm, Space, Tooltip } from 'antd'
 import { ICategoryBlog, IRoleUser } from '~/types'
 import { RootState, useAppDispatch } from '~/store/store'
 import { setCategoryBlog, setOpenDrawer } from '~/store/slices'
@@ -8,6 +8,11 @@ import { messageAlert } from '~/utils/messageAlert'
 import { pause } from '~/utils/pause'
 import { useAppSelector } from '~/store/hooks'
 import { useDeleteCategoryBlogMutation } from '~/store/services'
+import { useRef, useState } from 'react'
+import { FilterConfirmProps } from 'antd/es/table/interface'
+import { ColumnType } from 'antd/lib/table'
+import { SearchOutlined } from '@ant-design/icons'
+import Highlighter from 'react-highlight-words'
 
 export const useRenderCategoryBlog = () => {
   const dispatch = useAppDispatch()
@@ -23,6 +28,83 @@ export const useRenderCategoryBlog = () => {
       .then(() => messageAlert('Xóa thành công', 'success'))
   }
 
+  const [searchText, setSearchText] = useState('')
+  const [searchedColumn, setSearchedColumn] = useState('')
+  const searchInput = useRef<InputRef>(null)
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: ICategoryBlog
+  ) => {
+    confirm()
+    setSearchText(selectedKeys[0])
+    setSearchedColumn(`${dataIndex}`)
+  }
+
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters()
+    setSearchText('')
+  }
+
+  const getColumnSearchProps = (dataIndex: any): ColumnType<any> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Tìm kiếm danh mục`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <ButtonAntd
+            type='primary'
+            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            icon={<SearchOutlined />}
+          >
+            Search
+          </ButtonAntd>
+          <ButtonAntd
+            onClick={() => {
+              clearFilters && handleReset(clearFilters)
+            }}
+          >
+            Reset
+          </ButtonAntd>
+          <ButtonAntd
+            onClick={() => {
+              close()
+            }}
+          >
+            close
+          </ButtonAntd>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100)
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      )
+  })
   /* staff */
   const columnsStaff = [
     {
@@ -51,7 +133,8 @@ export const useRenderCategoryBlog = () => {
       title: 'Tên danh mục',
       dataIndex: 'name',
       key: 'name',
-      render: (name: string) => <p className='uppercase'>{name}</p>
+      render: (name: string) => <p className='uppercase'>{name}</p>,
+      ...getColumnSearchProps('name')
     },
 
     {
